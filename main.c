c8fee31f0fe2927a266fb2537a77cabe4b0878fd
Akram Khan <akrammiru@gmail.com>
2021-04-08 14:41:23 +0600

    refactored

--- main.c
+++ main.c
@@ -1,36 +1,37 @@
-#include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char *buffer;
     size_t buffer_length;
-    size_t input_length;
+    ssize_t input_length;
 } InputBuffer;
 
-void print_prompt() {
-    printf("db > ");
-}
-
 InputBuffer *new_input_buffer() {
-    InputBuffer *input_buffer = (InputBuffer *) malloc(sizeof(InputBuffer));
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
     input_buffer->buffer_length = 0;
     input_buffer->input_length = 0;
+
     return input_buffer;
 }
+
+void print_prompt() { printf("db > "); }
 
 void read_input(InputBuffer *input_buffer) {
     ssize_t bytes_read =
             getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-    if(bytes_read <= 0) {
+    if (bytes_read <= 0) {
         printf("Error reading input\n");
         exit(EXIT_FAILURE);
     }
+
+    // Ignore trailing newline
     input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read-1] = 0;
+    input_buffer->buffer[bytes_read - 1] = 0;
 }
 
 void close_input_buffer(InputBuffer *input_buffer) {
@@ -38,17 +39,16 @@ void close_input_buffer(InputBuffer *input_buffer) {
     free(input_buffer);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     InputBuffer *input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ".exit")==0) {
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
             close_input_buffer(input_buffer);
             exit(EXIT_SUCCESS);
-        }
-        else {
+        } else {
             printf("Unrecognized command '%s'.\n", input_buffer->buffer);
         }
     }
